6043da6a5f53cea046eeda036bac6515f5d7db8f
Nicolas Olivares <ne.olivaresgonzalez@gmail.com>
2019-07-01 21:57:38 -0400

    Added SearchForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SearchForm from './controller/SearchForm';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,16 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is app is powered by React + AWS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          BUSCADOR
+        </h3>
+        <SearchForm/>
       </header>
     </div>
   );
