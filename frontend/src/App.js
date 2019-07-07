6d965fac728181c90b15272df3596f47befa0d0d
Nicolas Olivares <ne.olivaresgonzalez@gmail.com>
2019-07-06 23:01:37 -0400

    Change on SearchForm.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchForm from './controller/SearchForm';
 import logo from './logo.svg';
 import './App.css';
--- frontend/src/controller/SearchForm.js
+++ frontend/src/controller/SearchForm.js
@@ -3,29 +3,51 @@ import React from 'react';
 class SearchForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {value: '', search_all: ''};
   
       this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleSubmit = this.find.bind(this);
+      
+
     }
   
     handleChange(event) {
       this.setState({value: event.target.value});
     }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+
+
+    find(event) {
+      //Request find() from API Gateway
+      fetch('https://17xl0wn9ga.execute-api.us-east-2.amazonaws.com/default/search_all',
+      { method: 'GET',
+               headers: new Headers(),
+               mode: 'cors',
+               cache: 'default' })
+	    .then((response) => {
+        alert('hola mundo')
+        return response.json()
+      })
+      .then((search_all) => {
+    	  console.log('OK')
+      })
+      .catch((error) => {
+        console.log('Hubo un problema con la petición Fetch:' + error.message);
+      });
     }
   
+
+    
     render() {
       return (
+        <div>
         <form onSubmit={this.handleSubmit}>
           <label>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
           <input type="submit" value="BUSCAR" />
         </form>
+        <ul>{this.state.search_all}</ul>
+        </div>
       );
     }
   }
